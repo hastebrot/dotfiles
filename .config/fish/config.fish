6cd1cc0697bc2e7a91c70ec9ee855d9ccb628745
Benjamin Gudehus <hastebrot@gmail.com>
2016-05-29 02:41:44 +0200

    (fix) fish: update for fish 2.3.0.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,25 @@
 # enable vi mode.
 
-fish_vi_mode
+#fish_vi_mode
+
+fish_vi_key_bindings
+set -g __fish_vi_mode 1
+
+#fish_vi_cursor
+#set fish_cursor_default block
+#set fish_cursor_insert line
+#set fish_cursor_visual block
 
 # set env variables.
 
 set -x PATH $PATH ~/.bin/
+set -x PATH $HOME/.cargo/bin $PATH
 set -Ux EDITOR nvim
 set -Ux VISUAL nvim
 
 # set command aliases.
 
+alias cd.. "cd .."
 alias ls "ls --color=auto"
 alias ll "ls -alF"
 
